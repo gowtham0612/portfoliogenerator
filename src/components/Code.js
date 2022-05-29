393fa2f9cfba8f7cddc653b49ff28eea4f5e797a
keerthivasan <keerthiva101@gmail.com>
2022-05-29 16:52:53 +0530

    download break

--- src/components/Code.js
+++ src/components/Code.js
@@ -97,7 +97,7 @@ const Code = ({
             <span class="text-primary">${LastName}</span>
           </h1>
           <div class="subheading mb-5">
-            ${Address} &bull; ${Phone} &bull;
+            ${Address} <br /> ${Phone} <br />
             <a href="mailto:${Email}">${Email}</a>
           </div>
           <p class="lead mb-5">${Description}</p>
